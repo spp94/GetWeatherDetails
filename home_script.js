f7e525a988fd3b2bd921d8e124c9aaebc28e35fb
spp94 <48171236+spp94@users.noreply.github.com>
2020-01-18 21:01:50 -0800

    Update home_script.js

--- home_script.js
+++ home_script.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+$(document).ready(function (){ 
     // function for on click of submit button to fetch weather data
     $("#btnSubmit").click(function () {
         getWeather(); // calling getWeather function, to fetch all the weather report for a city
